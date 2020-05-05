a4dbbeb09d838b297f695e6cba77a1f4f41ad285
eduardo mota <eduardolmota@hotmail.com>
2020-05-05 17:33:50 -0300

    Projeto Base

---
+++ Assets/Scripts/EspecificaçõesItens.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspecificacoesIten : MonoBehaviour
+{
+    public enum Classificacao
+    {
+        CorpoACorpo, ArmaADistancia, ArmaMagica
+    }
+
+    public enum Raridade
+    {
+        Comum, incomum, Raro, Lendario
+    }
+
+	public abstract class Item
+	{
+		protected string nome;
+		protected bool acumulavel;
+		protected Classificacao tipoDeItem;
+		protected Raridade raridadeDoItem;
+
+		public virtual void Examinar()
+		{
+			Debug.Log("Isto é um(a): " + nome);
+			string dados;
+			dados = string.Format("{0} é um(a) {1} {2}", nome, tipoDeItem, raridadeDoItem);//______ é um(a) _______ ________
+			Debug.Log(dados);
+		}
+
+		public abstract void RenomearItem(string novoNome);
+	}
+}
---
+++ Assets/Scripts/ProjetoBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProjetoBase : MonoBehaviour
+{
+    float dinheiro = 1500;
+
+    public bool compravel;
+
+    public Text dinheiros;
+
+    public Text pobre;
+
+    public float valorI;
+
+    public void AtualizarDinheiro()
+    {
+        dinheiros.text = dinheiro.ToString();
+    }
+
+    private void Awake()
+    {
+        pobre = GetComponent<Text>();
+        AtualizarDinheiro();
+    }
+
+    public void Selecionar(float valor)
+    {
+        valorI = valor;
+    }
+
+    public void Confirmar()
+    {
+        if (dinheiro < valorI)
+        {
+            pobre.enabled = true;
+            return;
+        }
+        dinheiro -= valorI;
+    }
+}
